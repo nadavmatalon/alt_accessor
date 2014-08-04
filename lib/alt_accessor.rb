b2bacbabadea5ca67b8d2fabb2cfb09004df760c
Nadav <nadav.matalon@gmail.com>
2014-08-04 04:30:23 +0100

    updated alt_accessor code

--- lib/alt_accessor.rb
+++ lib/alt_accessor.rb
@@ -5,18 +5,18 @@ module AltAccessor
 			args.each do |name|
 				if name.is_a?Symbol
 					define_method("#{name}") do
-		      			instance_variable_get("@#{name}")
-		 			end
+						instance_variable_get("@#{name}")
+					end
 					define_method("#{name}=") do |val|
-		      			instance_variable_set("@#{name}", val)
-		    		end
-		    		"attr_accessor methods created for: #{name}"
-		    	else
-		    		return "invalid argument: #{name} is not a symbol"
-		   		end
-		   	end
+						instance_variable_set("@#{name}", val)
+					end
+					return "attr_accessor methods created for: #{name}"
+				else
+					return "invalid argument: #{name} is not a symbol"
+				end
+			end
 		else
-			"invalid argument: method requires a symbol as an argument"
+			return "invalid argument: method requires a symbol as an argument"
 		end
 	end	
 end
